b8b4d28f382d7ec966961faa5ac59bea4b687936
okoedo trust <okoedotrust22@gmail.com>
2024-03-30 14:51:41 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+toggle_btn = document.querySelector(".toggle-menu");
+flex = document.querySelector(".head_flex{");
+
+toggle_btn.addEventListener("click",()=>{
+    flex.setAttribute("visibilty", "visible")
+})
