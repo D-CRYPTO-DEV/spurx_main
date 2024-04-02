dce1d2abb0b915389c3f65bc4152ad98185b0e80
okoedo trust <okoedotrust22@gmail.com>
2024-04-01 20:44:09 -0700

    my masterpiece

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 toggle_btn = document.querySelector(".toggle-menu");
 flex = document.querySelector(".nav-menu");
+dropDown = document.querySelector(".drop-head");
+main = document.querySelector("main");
+cancelBTN = document.querySelector(".cancel-op");
 
 toggle_btn.addEventListener("click",()=>{
-    flex.style.transform = translate("40px");
-    flex.classList.toggle("nav-menu2");
+    main.classList.add("main2");
+    dropDown.classList.add("drop-head2");
+})
+cancelBTN.addEventListener("click",()=>{
+    main.classList.remove("main2");
+    dropDown.classList.remove("drop-head2");
 })
