adc80dfaab4f59148c378aea9fda6835cbd9f84f
okoedo trust <okoedotrust22@gmail.com>
2024-04-04 05:15:51 -0700

    spurt landing page

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ flex = document.querySelector(".nav-menu");
 dropDown = document.querySelector(".drop-head");
 main = document.querySelector("main");
 cancelBTN = document.querySelector(".cancel-op");
+bodyHidder = document.querySelector("body");
 
 toggle_btn.addEventListener("click",()=>{
-    main.classList.add("main2");
     dropDown.classList.add("drop-head2");
+    bodyHidder.classList.add("body-hidder")
 })
 cancelBTN.addEventListener("click",()=>{
-    main.classList.remove("main2");
     dropDown.classList.remove("drop-head2");
+    bodyHidder.classList.remove("body-hidder")
 })
